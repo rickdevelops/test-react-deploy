7656c4f2a2c48d1111298e6e6fe0a47cbbb76036
Sounak Dey <sounakdeyrick1@gmail.com>
2021-12-16 17:11:12 +0530

    updated configs

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,4 +1,6 @@
-exports.NUMBER_OF_QUESTIONS_IN_A_PAGE = 5;
-exports.SHUFFLE_QUESTIONS = true;
-exports.SHUFFLE_OPTIONS = true;
-exports.BACKEND_URL = "https://questionbankbackend.herokuapp.com";
+exports.NUMBER_OF_QUESTIONS_IN_A_PAGE =
+  process.env.NUMBER_OF_QUESTIONS_IN_A_PAGE || 5;
+exports.SHUFFLE_QUESTIONS = process.env.SHUFFLE_QUESTIONS || true;
+exports.SHUFFLE_OPTIONS = process.env.SHUFFLE_OPTIONS || true;
+exports.BACKEND_URL =
+  process.env.BACKEND_URL || "https://questionbankbackend.herokuapp.com";
