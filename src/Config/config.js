03c24628367f6d5f98730cb33c6bd72b64f20b25
Sounak Dey <sounakdeyrick1@gmail.com>
2021-12-16 16:27:05 +0530

    updated backend url

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,4 +1,4 @@
 exports.NUMBER_OF_QUESTIONS_IN_A_PAGE = 5;
 exports.SHUFFLE_QUESTIONS = true;
 exports.SHUFFLE_OPTIONS = true;
-exports.BACKEND_URL = "http://questionbankbackend.herokuapp.com";
+exports.BACKEND_URL = "https://questionbankbackend.herokuapp.com";
